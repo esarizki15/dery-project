654d887aaf81cf3573d0bdf1d674cbec0daf8d87
Esa Rizki <esa.rizki.hari.utama@altomatik.com>
2022-01-07 18:45:28 +0700

    up

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -16,11 +16,44 @@ class UserSeeder extends Seeder
         $role->name = "Admin";
         $role->save();
 
+        $role = new Role();
+        $role->name = "Kepala Bagian";
+        $role->save();
+
+        $role = new Role();
+        $role->name = "Kepala Divisi";
+        $role->save();
+
+        $role = new Role();
+        $role->name = "Pegawai";
+        $role->save();
+
         $user = new User();
         $user->name = "Admin Laravel";
         $user->email = "admin@gmail.com";
         $user->password = bcrypt("rahasia");
         $user->role_id = $role->id; 
+        $user->save();
+
+        $user = new User();
+        $user->name = "Kepala Bagian";
+        $user->email = "kepala_bagian@gmail.com";
+        $user->password = bcrypt("rahasia");
+        $user->role_id = 2; 
+        $user->save();
+
+        $user = new User();
+        $user->name = "Kepala Divisi";
+        $user->email = "kepala_divisi@gmail.com";
+        $user->password = bcrypt("rahasia");
+        $user->role_id = 3; 
+        $user->save();
+
+        $user = new User();
+        $user->name = "Pegawai";
+        $user->email = "pegawai@gmail.com";
+        $user->password = bcrypt("rahasia");
+        $user->role_id = 4; 
         $user->save();
     }
 }
